4097a3be9f5ba3b3563d1c5ad2eee17b37cff331
doyeon-mun <doyeon.mun@linecorp.com>
2022-04-04 20:13:35 +0900

    Init spring boot

---
+++ src/main/java/com/example/armeriacircuitbreakerwithokhttpclient/ArmeriaCircuitbreakerWithOkhttpClientApplication.java
@@ -0,0 +1,13 @@
+package com.example.armeriacircuitbreakerwithokhttpclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArmeriaCircuitbreakerWithOkhttpClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArmeriaCircuitbreakerWithOkhttpClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/armeriacircuitbreakerwithokhttpclient/ArmeriaCircuitbreakerWithOkhttpClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.armeriacircuitbreakerwithokhttpclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArmeriaCircuitbreakerWithOkhttpClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
